37e1547930c233e9a8e1c826326cbdbe9d315437
Juan Carlos Ramirez <juramir@protonmail.com>
2023-06-27 09:24:48 -0500

    Update mac install script

--- setup/mac-install.sh
+++ setup/mac-install.sh
@@ -6,39 +6,26 @@ brew install --cask 1password
 brew install --cask iterm2
 brew install --cask nextcloud
 brew install --cask docker
-brew install --cask burp-suite
-brew install --cask ghidra
-brew install --cask arduino
-brew install --cask virtualbox
 brew install --cask discord
-brew install --cask ghidra
 brew install --cask spotify
 brew install lens
 brew install visual-studio-code
 brew install python3
 brew install binwalk
 brew install wget
-brew install gobuster
-brew install hydra
-brew install hashcat
-brew install spoof-mac
-brew install aircrack-ng
-brew install nmap
-brew install irssi
 brew install gpg
 brew install certutil
 brew install libusb
 brew install cmake
 brew install nmake
-#brew install macchanger
 
 # These require password auth, post install
-brew install --cask tunnelblick
+brew install --cask openvpn-connect
+#brew install --cask tunnelblick
 brew install --cask wireshark
 
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-
 
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
@@ -55,3 +42,21 @@ python3 get-pip.py
 rm get-pip.py
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Install Downlink and Amphetamine from the App Store"
+
+#Commented items are for sec testing
+# Consider controlling these installs from a flag
+#brew install --cask burp-suite
+#brew install --cask ghidra
+#brew install --cask arduino
+#brew install --cask virtualbox
+#brew install --cask ghidra
+#brew install gobuster
+#brew install hydra
+#brew install hashcat
+#brew install spoof-mac
+#brew install aircrack-ng
+#brew install nmap
+#brew install irssi
+#brew install macchanger
